a0dc52804875a23f5ef292e0197ee8217b4f552f
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-06 05:38:56 -0400

    Completed the Week 12 Lab

--- src/app/order-form/order-form.ts
+++ src/app/order-form/order-form.ts
@@ -11,12 +11,22 @@ import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 export class OrderForm {
   purchaseForm : any;
 
+  protected readonly ticketTypes = [
+    { value: 'Adult', label: 'Adult (18-64)' },
+    { value: 'Youth', label: 'Youth (13+)' },
+    { value: 'Child', label: 'Child (1-12)' },
+    { value: 'Senior', label: 'Senior (65+)' }
+  ];
+
   constructor(private formBuilder: FormBuilder) {}
 
     ngOnInit() : void {
       this.purchaseForm = this.formBuilder.group({
-        name: ['', [Validators.required, Validators.minLength(3)]]
-        // email: ['', [Validators.required, Validators.email]],
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+        ticketType: ['', [Validators.required]],
+        numTickets: ['', [Validators.required, Validators.min(1), Validators.max(10)]]
         // product: ['', Validators.required],
         // quantity: [1, [Validators.required, Validators.min(1)]]
       });
@@ -24,11 +34,13 @@ export class OrderForm {
 
     submitForm() : void {
 
-  if (this.purchaseForm.invalid) {
-    this.purchaseForm.markAllAsTouched();  
-    return;
-  }
+      if (this.purchaseForm?.valid) {
+      alert(`Form data: ${JSON.stringify(this.purchaseForm.value)}`);  
+  } else {
 
-  alert(`Form data: ${JSON.stringify(this.purchaseForm.value)}`);
-}
+      this.purchaseForm.markAllAsTouched();
+
+      alert('Please enter all the required inputs correctly.');
+    }
+  }
 }
